d976df2eedb752a71743df6debe193893e3a225d
Hemel Bourhan <hemelbourhan@gmailgit config --global user.email Bourhan>
2023-03-21 11:37:05 +0600

    add capital and region

--- js/countries.js
+++ js/countries.js
@@ -6,20 +6,42 @@ const loadAllCountries =()=>{
 }
 
 const showAllCountries = countries =>{
-    //   console.log(countries);
-    const countriesHTML = countries.map(country => getCountryHTML(country))
+      console.log(countries);
+    const countriesHTML = countries.map(country => getCountryHTML(country))   
     // console.log(countriesHTML[0]);
     const container = document.getElementById('countries');
     container.innerHTML=countriesHTML.join(' ');
     
 }
-const getCountryHTML = country =>{
+const getCountryHTML =( {name , flags , capital,region} ) =>{
+    
     return ` 
         <div class="country">
-            <h2> ${country.name.common}</h2>
-            <img src="${country.flags.png}" alt="">
+            <h2> ${name.common}</h2>
+            <h2 id="capital" > Capital : ${capital}</h2>
+            <h2 id="capital" > Continent : ${region}</h2>
+            <img src="${flags.png}" alt="">
         </div>
     `
 }
+// const getCountryHTML =( country ) =>{
+    // original 
+//     return ` 
+//         <div class="country">
+//             <h2> ${country.name.common}</h2>
+//             <img src="${country.flags.png}" alt="">
+//         </div>
+//     `
+// }
+// const getCountryHTML =( country ) =>{
+//     // option-1
+//     const {name , flags} = country;
+//     return ` 
+//         <div class="country">
+//             <h2> ${name.common}</h2>
+//             <img src="${flags.png}" alt="">
+//         </div>
+//     `
+// }
 
 loadAllCountries()
